49aebcb70d07466c93fe28d5c1e396ef3b5c4b7c
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2023-02-20 10:42:39 +0300

    Add files via upload

---
+++ labwork1/Filters.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel;
+
+
+namespace labwork1
+{
+    public abstract class Filters
+    {
+        public int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            for(int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)(float)i / resultImage.Width * 100);
+                if (worker.CancellationPending)
+                    return null;
+                for(int j = 0; j < sourceImage.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j)); 
+                }
+            }
+            return resultImage;
+        }
+
+    }
+    public class InvertFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
+            return resultColor;
+        }
+    } 
+}
